6a74e0761751d61991f4dee7dc81376de4c76b1e
simrett <leesim242@gmail.com>
2023-11-20 14:53:05 -0800

    modified

--- models/place.py
+++ models/place.py
@@ -39,21 +39,20 @@ class Place(BaseModel, Base):
                                  back_populates="place_amenities")
 
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+        reviews_id = []
 
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
-    reviews_id = []
-
-    @property
+        @property
         def reviews(self):
             """ Returns list of reviews.id """
             var = models.storage.all()
