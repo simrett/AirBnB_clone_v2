9c2a35769c0b46f161eb3cb365ec75cb0c3ec74b
simrett <leesim242@gmail.com>
2023-11-20 14:44:41 -0800

    modified

--- models/amenity.py
+++ models/amenity.py
@@ -8,6 +8,7 @@ import os
 if os.getenv("HBNB_TYPE_STORAGE") == 'db':
     from models.place import place_amenity
 
+
 class Amenity(BaseModel, Base):
     """Amenity inherits from BaseModel and Base"""
     if os.getenv("HBNB_TYPE_STORAGE") == 'db':
@@ -15,4 +16,4 @@ class Amenity(BaseModel, Base):
         name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary=place_amenity)
     else:
-    name = ""
+        name = ""
