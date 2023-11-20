77efaafc8cd3cb00a8a7a25a490b692e71c74e8d
simrett <leesim242@gmail.com>
2023-11-20 14:49:23 -0800

    added system path appending

--- models/city.py
+++ models/city.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class City(BaseModel):
     """ The city class, contains state ID and name """
 
