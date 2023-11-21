2616b9a6bc6833a16e4440074f2d167be0e0e806
simrett <leesim242@gmail.com>
2023-11-20 17:31:12 -0800

    Impliment User class

--- models/user.py
+++ models/user.py
@@ -1,30 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from os import getenv
 
 
-class User(BaseModel, Base):
+class User(BaseModel):
     """This class defines a user by various attributes"""
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", cascade="delete", backref="user")
-        reviews = relationship("Review", cascade="delete", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """
-            Initialize user instance
-        """
-        super().__init__(*args, **kwargs)
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
