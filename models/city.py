04d0cc7176baf9f1cfc1ccd76ec645eadd65ebd2
simrett <leesim242@gmail.com>
2023-11-20 17:38:43 -0800

    Correct style errors

--- models/city.py
+++ models/city.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from os import getenv
+from models.base_model import BaseModel
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        __tablename__ = "cities"
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship("Place", cascade='delete', backref="cities")
-    else:
-        state_id = ""
-        name = ""
-        places = []
+    state_id = ""
+    name = ""
