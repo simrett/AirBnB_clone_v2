c2aaedfc58aeecac6c4643ec324812001565d41e
MphoCM <christian.mofokeng@gmail.com>
2023-11-21 07:39:14 -0800

    for dbstorage test

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""tests file storage"""
+import unittest
+import pep8
+
+
+class TestDBstorage(unittest.TestCase):
+    """tests DBstorage engine"""
+
+    def setUp(self):
+        """SetUp env for test"""
+        pass
+
+    def tearDown(self):
+        """teardown"""
+        pass
+
+    def test_all(self):
+        """Test class method all()"""
+        pass
+
+    def test_new(self):
+        """Test new method"""
+        pass
+
+    def test_save(self):
+        """Test save method
+        """
+        pass
+
+    def test_delete(self):
+        """Tests delete
+        """
+        pass
+
+    def test_reload(self):
+        """Tests reload
+        """
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
