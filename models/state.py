4e6e6a2f34801ede059207630f9e3db4c4df746e
simrett <leesim242@gmail.com>
2023-11-21 07:49:32 -0800

    fixed

--- models/state.py
+++ models/state.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class """
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
-import models
-from os import getenv
+from os import environ
 
 
 class State(BaseModel, Base):
-    """ State class initialization """
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="delete", backref="state")
+    """class for state
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
 
-    else:
-        name = ""
+    cities = relationship("City",
+                          backref="state",
+                          cascade="all, delete-orphan",
+                          passive_deletes=True)
 
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """get list of city"""
-            city_ls = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_ls.append(city)
-            return city_ls
+            return [city for city in models.storage.all(
+                City).values() if city.state_id == self.id]
